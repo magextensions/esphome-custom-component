99cbb9c18a8860fe9ab0566d40a90fbd6945d229
Robert Klep <robert@klep.name>
2026-03-21 08:06:18 +0100

    Cannot call App.register_component() from component ctor anymore

--- components/custom_component/custom_component.h
+++ components/custom_component/custom_component.h
@@ -12,10 +12,6 @@ class CustomComponentConstructor {
  public:
   CustomComponentConstructor(const std::function<std::vector<Component *>()> &init) {
     this->components_ = init();
-
-    for (auto *comp : this->components_) {
-      App.register_component(comp);
-    }
   }
 
   Component *get_component(int i) const { return this->components_[i]; }
